569d73754cb3ddb84d4373b37c86fb52a3cad0e1
HilbertXu <hilbertxu@outlook.com>
2019-02-07 18:33:00 +0800

    Update 2019/02/07 wave detect part

--- kamerider_image_detection/src/wave_detect.py
+++ kamerider_image_detection/src/wave_detect.py
@@ -20,7 +20,7 @@ from cv_bridge import CvBridge, CvBridgeError
 from kamerider_image_msgs.msg import BoundingBox
 from kamerider_image_msgs.msg import FaceDetection
 #openpose part
-import openpose as op 
+from openpose import pyopenpose as op
 
 class wave_detect:
     def __init__(self):
@@ -47,17 +47,36 @@ class wave_detect:
         rospy.Subscriber(self.sub_image_raw_topic_name, Image, self.imageCallback)
         self.pub_result = rospy.Publisher(self.pub_wave_detect_topic_name, FaceDetection, queue_size=1)
         #设置openpose手部特征点检测时的参数
-        self.params["model_folder"] = "../../../models/"
+        self.params["model_folder"] = "/home/kamerider/openpose/models"
         self.params["hand"] = True
         self.params["hand_detector"] = 2
         self.params["body_disable"] = True
 
     def rect_to_bbox(self, rect):
+        '''
+        (xmin, ymin)---------
+        |                   |
+        |                   |
+        |                   |
+        |---------(xmax, ymax)
+        '''
         x_min = rect.left()
         y_min = rect.top()
         x_max = rect.right()
         y_max = rect.bottom()
         return (x_min, y_min, x_max, y_max)
+    
+    def face_region_zoom_out(self, rect):
+        rect_coords = self.rect_to_bbox(rect)
+        xmin = rect_coords[0]-100
+        ymin = rect_coords[1]-50
+        xmax = rect_coords[0]
+        ymax = rect_coords[3]+50
+        handRectangle = [
+            op.Rectangle(rect_coords[0]-100, rect_coords[1]-50, 100, 150),
+            op.Rectangle(rect_coords[2], rect_coords[1]-50, 100, 150)
+        ]
+        return handRectangle
 
     def imageCallback(self, msg):
         print ("[INFO] Receiving image")
@@ -76,18 +95,37 @@ class wave_detect:
         detector  = dlib.get_frontal_face_detector()
         #利用检测器检测人脸
         rects = detector(gray_image, 2)
+        handRectangles=[]
         #获得人脸在图像中的位置之后，将人脸两侧区域放大，然后在这个放大了的区域内检测手部
-
-    def face_region_zoom_out(self, rects):
         for rect in rects:
-            rect_coords = self.rect_to_bbox(rect)
-            xmin = rect_coords[0]-100
-            ymin = rect_coords[1]-50
-            xmax = rect_coords[0]
-            ymax = rect_coords[3]+50
-            handRectangle = [
-                op.Rectangle(rect_coords[0]-100, rect_coords[1]-50, 100, 150),
-                op.Rectangle(rect_coords[2], rect_coords[1]-50, 100, 150)
-                ]
+            handRegion = self.face_region_zoom_out(rect)
+            handRectangles.append(handRegion)
+        self.openpose_hand_detect(handRectangles, cv_image)
+    
+    def openpose_hand_detect(self, handRectangles, cv_image):
+        # Starting OpenPose
+        opWrapper = op.WrapperPython()
+        opWrapper.configure(self.params)
+        opWrapper.start()
+
+        # Create new datum
+        datum = op.Datum()
+        datum.cvInputData = cv_image
+        datum.handRectangles = handRectangles
+
+        # Process and display image
+        opWrapper.emplaceAndPop([datum])
+        print("Left hand keypoints: \n" + str(datum.handKeypoints[0]))
+        print("Right hand keypoints: \n" + str(datum.handKeypoints[1]))
+        cv2.imshow("OpenPose 1.4.0 - Tutorial Python API", datum.cvOutputData)
+        cv2.waitKey(0)
+
+if __name__ == '__main__':
+    rospy.init_node('wave_detect')
+    print ('----------init----------')
+    print ('-----WAITING FOR IMAGE-----')
+    wave_detect()
+    rospy.spin()
+
 
 
