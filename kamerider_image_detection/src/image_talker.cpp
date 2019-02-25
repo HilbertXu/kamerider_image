e8c5cfd760b59c407cfe6abdd69130ace8af38e5
HilbertXu <hilbertxu@outlook.com>
2019-02-25 19:46:05 +0800

    Update 2019/02/25

--- kamerider_image_detection/src/image_talker.cpp
+++ kamerider_image_detection/src/image_talker.cpp
@@ -29,9 +29,9 @@ int main(int argc, char** argv)
     ros::init (argc, argv, "image_talker");
     ros::NodeHandle nh;
     image_transport::ImageTransport it(nh);
-    image_transport::Publisher pub = it.advertise("/image_raw", 1);
+    image_transport::Publisher pub = it.advertise("/camera/rgb/image_raw", 1);
 
-    cv::Mat image = cv::imread("/home/kamerider/Pictures/face.jpg", CV_LOAD_IMAGE_COLOR);
+    cv::Mat image = cv::imread("/home/kamerider/Pictures/right.png", CV_LOAD_IMAGE_COLOR);
     sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
 
     ros::Rate loop_rate(5);
